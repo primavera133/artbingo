ea478d456fa06a7729da320cf3c8ad13808a3dd7
Jonas Myrenås <jonas.myrenas@vinnovera.se>
2018-01-22 18:26:29 +0100

    dotenv for sagas

--- artbingo/saga.js
+++ artbingo/saga.js
@@ -1,49 +1,50 @@
 /* global fetch */
 
-import {all, call, put, take, takeLatest} from 'redux-saga/effects'
+import { all, call, put, take, takeLatest } from 'redux-saga/effects'
 import es6promise from 'es6-promise'
 import 'isomorphic-fetch'
 
-import {actionTypes, failure, loadBingoGamesSuccess, loadBingoGameSuccess, loadDataSuccess} from './actions'
+import { actionTypes, failure, loadBingoGamesSuccess, loadBingoGameSuccess, loadDataSuccess } from './actions'
 
 es6promise.polyfill()
+const baseUrl = `${process.env.PROTOCOL}://${process.env.DOMAIN}:${process.env.PORT}`
 
-function* loadDataSaga() {
-  try {
-    const res = yield fetch('http://localhost:3000/api/species')
-    const data = yield res.json()
-    yield put(loadDataSuccess(data))
-  } catch (err) {
-    yield put(failure(err))
-  }
+function* loadDataSaga () {
+	try {
+		const res = yield fetch(`${baseUrl}/api/species`)
+		const data = yield res.json()
+		yield put(loadDataSuccess(data))
+	} catch (err) {
+		yield put(failure(err))
+	}
 }
 
-function* loadBingoGamesSaga() {
-  try {
-    const res = yield fetch('http://localhost:3000/api/games')
-    const data = yield res.json()
-    yield put(loadBingoGamesSuccess(data))
-  } catch (err) {
-    yield put(failure(err))
-  }
+function* loadBingoGamesSaga () {
+	try {
+		const res = yield fetch(`${baseUrl}/api/games`)
+		const data = yield res.json()
+		yield put(loadBingoGamesSuccess(data))
+	} catch (err) {
+		yield put(failure(err))
+	}
 }
 
-function* loadBingoGameSaga(params) {
-  try {
-    const res = yield fetch(`http://localhost:3000/api/game/${params.payload}`)
-    const data = yield res.json()
-    yield put(loadBingoGameSuccess(data))
-  } catch (err) {
-    yield put(failure(err))
-  }
+function* loadBingoGameSaga (params) {
+	try {
+		const res = yield fetch(`${baseUrl}/api/game/${params.payload}`)
+		const data = yield res.json()
+		yield put(loadBingoGameSuccess(data))
+	} catch (err) {
+		yield put(failure(err))
+	}
 }
 
-function* rootSaga() {
-  yield all([
-    takeLatest(actionTypes.LOAD_DATA, loadDataSaga),
-    takeLatest(actionTypes.LOAD_BINGO_GAMES, loadBingoGamesSaga),
-    takeLatest(actionTypes.LOAD_BINGO_GAME, loadBingoGameSaga)
-  ])
+function* rootSaga () {
+	yield all([
+		takeLatest(actionTypes.LOAD_DATA, loadDataSaga),
+		takeLatest(actionTypes.LOAD_BINGO_GAMES, loadBingoGamesSaga),
+		takeLatest(actionTypes.LOAD_BINGO_GAME, loadBingoGameSaga)
+	])
 }
 
 export default rootSaga
